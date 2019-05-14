9f808d6eaedd51b5ffcbe7fc0bff535a6d58e1e4
miiibdboy <50560477+miiibdboy@users.noreply.github.com>
2019-05-14 19:53:14 +0200

    Add files via upload

---
+++ u.js
@@ -0,0 +1,61 @@
+require('dotenv').config();
+const { XIAO_TOKEN, OWNERS, XIAO_PREFIX, INVITE } = process.env;
+const path = require('path');
+const Client = require('./structures/Client');
+const client = new Client({
+	commandPrefix: *,
+	owner: OWNERS.split('404596433400889344'),
+	invite: INVITE,
+	disableEveryone: true,
+	disabledEvents: ['TYPING_START']
+});
+const activities = require('./assets/json/activity');
+
+client.registry
+	.registerDefaultTypes()
+	.registerTypesIn(path.join(__dirname, 'types'))
+	.registerGroups([
+		['util', 'Utility'],
+		['info', 'Discord Information'],
+		['random', 'Random Response'],
+		['single', 'Single Response'],
+		['events', 'Events'],
+		['search', 'Search'],
+		['analyze', 'Analyzers'],
+		['games', 'Games'],
+		['image-edit', 'Image Manipulation'],
+		['avatar-edit', 'Avatar Manipulation'],
+		['text-edit', 'Text Manipulation'],
+		['number-edit', 'Number Manipulation'],
+		['other', 'Other'],
+		['roleplay', 'Roleplay']
+	])
+	.registerDefaultCommands({
+		help: false,
+		ping: false,
+		prefix: false,
+		commandState: false,
+		unknownCommand: false
+	})
+	.registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.on('ready', () => {
+	client.logger.info(`[READY] Logged in as ${client.user.tag}! ID: ${client.user.id}`);
+	client.setInterval(() => {
+		const activity = activities[Math.floor(Math.random() * activities.length)];
+		client.user.setActivity(activity.text, { type: activity.type });
+	}, 60000);
+});
+
+client.on('disconnect', event => {
+	client.logger.error(`[DISCONNECT] Disconnected with code ${event.code}.`);
+	process.exit(0);
+});
+
+client.on('error', err => client.logger.error(err));
+
+client.on('warn', warn => client.logger.warn(warn));
+
+client.on('commandError', (command, err) => client.logger.error(`[COMMAND:${command.name}]\n${err.stack}`));
+
+client.login(BOT_TOKEN);
